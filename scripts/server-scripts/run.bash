e45d7ef7e831cb743e608b055c20aa090eb3cc49
chanyoungs-SP7 <chanyoungs@gmail.com>
2025-02-05 15:57:56 +0900

    transfer server scripts

--- scripts/server-scripts/run.bash
+++ scripts/server-scripts/run.bash
@@ -1,2 +1,2 @@
 screen -ls | grep -q "train" || screen -dmS train
-screen -S train -X stuff "bash /home/chan/train.bash $1\n"
+screen -S train -X stuff "bash /home/chan/server-scripts/train.bash $1\n"
