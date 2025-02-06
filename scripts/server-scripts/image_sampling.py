6886647232e56a9e192e5cad3b2abdfcce0b01f0
chanyoungs-SP7 <chanyoungs@gmail.com>
2025-02-06 11:16:37 +0900

    sampling density added

---
+++ scripts/server-scripts/image_sampling.py
@@ -0,0 +1,38 @@
+import os
+import json
+import argparse
+import shutil
+
+parser = argparse.ArgumentParser(description="Update file paths in a JSON file.")
+parser.add_argument("--data_dir", type=str, help="Directory to the data folder")
+parser.add_argument("--sampling_density", type=float, help="Sampling density value")
+args = parser.parse_args()
+
+TRANSFORMS_PATH_ORIGINAL = os.path.join(args.data_dir, "transforms_original.json")
+TRANSFORMS_PATH_NEW = os.path.join(args.data_dir, "transforms.json")
+
+if not os.path.exists(TRANSFORMS_PATH_NEW):
+    shutil.copy(TRANSFORMS_PATH_NEW, TRANSFORMS_PATH_NEW+"_original")
+
+if args.sampling_density >= 1:
+    shutil.copy(TRANSFORMS_PATH_ORIGINAL, TRANSFORMS_PATH_NEW)
+else:
+    # Load the JSON data from the file
+    with open(TRANSFORMS_PATH_ORIGINAL, "r") as file:
+        data = json.load(file)
+
+    frames_original = len(data["frames"])
+    frames = []
+    # Update the file_path values
+    for n, frame in enumerate(data["frames"]):
+        if n % int(1 / args.sampling_density) == 0:
+            frames.append(frame)
+
+    data["frames"] = frames
+    frames_new = len(data["frames"])
+
+    # Save the modified data back to the JSON file
+    with open(TRANSFORMS_PATH_NEW, "w") as file:
+        json.dump(data, file, indent=4)
+
+    print(f"Sampling density({args.sampling_density}) updated successfully: {frames_original} -> {frames_new} frames.")
--- scripts/server-scripts/train.bash
+++ scripts/server-scripts/train.bash
@@ -2,6 +2,19 @@ OUTPUT_DIR=/home/chan/outputs
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate nerfstudio
 
+DATA_DIR=$1
+PROCESS_DATA_BOOL=$2
+SAMPLING_DENSITY=$3
+
+if [ "$PROCESS_DATA_BOOL" = "True" ]; then
+    ns-process-data images --data $DATA_DIR --output-dir ${DATA_DIR}_colmap
+    DATA_DIR=${DATA_DIR}_colmap
+fi
+
+if [ -n "$SAMPLING_DENSITY" ]; then
+    python ~/server-scripts/image_sampling.py --data_dir $DATA_DIR --sampling_density $SAMPLING_DENSITY
+fi
+
 ns-train splatfacto-mcmc \
 --viewer.quit-on-train-completion=True \
 --vis viewer+tensorboard \
@@ -9,4 +22,4 @@ ns-train splatfacto-mcmc \
 --steps-per-save=1000 \
 --save-only-latest-checkpoint=False \
 --output-dir $OUTPUT_DIR \
---data $1
+--data $DATA_DIR
